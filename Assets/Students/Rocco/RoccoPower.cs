7ba2fcab9c05e718cab86d41df38fb8dfd27c02e
ROCCO-PC\Rocco <Rocco.Russo@my.liu.edu>
2022-02-02 16:21:51 -0500

    Can I push?

--- Assets/Students/Rocco/RoccoPower.cs
+++ Assets/Students/Rocco/RoccoPower.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoccoPower : GenericPower
-{
-    public override void Activate()
-    {
-        
-    }
+public class RoccoPower : GenericPower {
+	public override void Activate() {
+		Debug.Log("Is someone talkin' ova thea?");
+	}
 
-    void Update()
-    {
-        
-    }
+	void Update() {
+
+	}
 }
