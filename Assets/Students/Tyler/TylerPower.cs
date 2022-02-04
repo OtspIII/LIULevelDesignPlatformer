a37fd7457eb1bbc6fe2a6b37ead2e1a8af69cc2a
D3VILus <98358862+D3VILus@users.noreply.github.com>
2022-02-04 16:41:59 -0500

    no message

--- Assets/Students/Tyler/TylerPower.cs
+++ Assets/Students/Tyler/TylerPower.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class TylerPower : GenericPower
 {
     public float Timer = 0;
-
+    public bool xAllowed = false;
+    bool teleporty,isTouchy;
+    public GameObject fivemeters;
     public override void Activate()
     {
         Timer = 1;
@@ -15,6 +17,9 @@ public class TylerPower : GenericPower
 
     void Update()
     {
+        isTouchy = fivemeters.GetComponent<FiveMeterCollisionScript>().isTouchy();
+        Debug.Log(isTouchy);
+        fivemeters.transform.position=new Vector2(transform.position.x+5, transform.position.y);
         if (Timer > 0)
         {
             Timer -= Time.deltaTime / 0.5f;
@@ -26,5 +31,31 @@ public class TylerPower : GenericPower
                 Player.SetInControl(true);
             }
         }
+
+
+        if (Input.GetKeyDown(KeyCode.X) && teleporty)
+        {
+            transform.position = new Vector2(transform.position.x+5,transform.position.y);
+        }
     }
+
+
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right,5);
+        Debug.DrawRay(transform.position, Vector2.right*5, Color.yellow);
+        Debug.Log(hit.collider.tag);
+        if (isTouchy==false)
+        {
+            teleporty = true;
+        }
+        else
+        {
+            teleporty = false;
+        }
+        
+    }
+
+   
 }
