7e7b08632259984082bf9107cbb0cba12c67c610
Misha <otspiii@gmail.com>
2022-02-28 17:17:24 -0500

    fixed a bug where health would reset between levels

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -76,7 +76,8 @@ public class CharController : MonoBehaviour
         SR.color = c;
         float skinny = data.Type == MTypes.Leaper ? 0.25f : 0.5f;
         transform.localScale = new Vector3(data.Size*0.5f,data.Size*skinny,1);
-        HP = data.HP;
+        if (HP == 0) HP = data.HP;
+        if (data.Color == MColors.Player) Debug.Log(data.Color);
     }
     
     public virtual void TakeDamage(int amt)
@@ -103,7 +104,7 @@ public class CharController : MonoBehaviour
     public virtual void Shoot()
     {
         if (BulletCooldown < Data.AttackRate) return;
-        Debug.Log("PEW: " + Time.time);
+        //Debug.Log("PEW: " + Time.time);
         Vector3 rot = transform.rotation.eulerAngles;
         Vector3 pos = transform.position + (transform.right * transform.localScale.x * -0.5f);
         if (Data.AttackSpread > 0) rot.z += Random.Range(0, Data.AttackSpread) - (Data.AttackSpread / 2);
